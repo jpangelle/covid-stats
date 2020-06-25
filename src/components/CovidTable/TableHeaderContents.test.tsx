9f27a1345d2ea515c3ea1ebb1607ab5f5a7b09d9
JP Angelle <jp@angelle.me>
2020-06-24 20:56:05 -0500

    add tests for header contents

--- src/components/CovidTable/TableHeaderContents.test.tsx
+++ src/components/CovidTable/TableHeaderContents.test.tsx
@@ -3,11 +3,30 @@ import { render } from '@testing-library/react';
 import { TableHeaderContents } from './TableHeaderContents';
 
 describe('Covid Table Header Contents', () => {
-  it('should render the header contents with sort icon', () => {
+  it('should render the header contents with asc sort icon', () => {
     const props = {
       columnDisplayName: 'Population',
       columnName: 'population',
       sortedBy: 'asc',
+      sortedColumn: 'population',
+    };
+
+    const { getByTestId, getByText } = render(
+      <TableHeaderContents {...props} />,
+    );
+
+    const populationHeader = getByText('Population');
+    expect(populationHeader).toBeInTheDocument();
+
+    const sortIcon = getByTestId('sort-icon');
+    expect(sortIcon).toBeInTheDocument();
+  });
+
+  it('should render the header contents with desc sort icon', () => {
+    const props = {
+      columnDisplayName: 'Population',
+      columnName: 'population',
+      sortedBy: 'desc',
       sortedColumn: 'population',
     };
 
